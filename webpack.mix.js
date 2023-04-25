4130814b2a1702bac05d57d73015a85802a2cfd2
Vento <github@brenneisen.me>
2023-04-25 22:48:38 +0200

    Update webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,4 @@
 const mix = require('laravel-mix');
 
-mix.js('src/frontend/assets/js/app.js', 'src/frontend/js/lib');
+mix.js('src/frontend/assets/js/app.js', 'src/frontend/js/lib')
+    .sass('src/frontend/assets/scss/app.scss', 'src/frontend/css/lib');
