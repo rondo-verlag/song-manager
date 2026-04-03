f544079fc0b1907577956f26ec34503d8400e5ff
Demian Holderegger <demian.holderegger@gmail.com>
2026-04-03 16:56:42 +0200

    fix icons in build

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
 	plugins: [vue()],
 	root: 'src/frontend',
+	base: '/frontend/dist/',
 	publicDir: false,
 	build: {
 		outDir: 'dist',
